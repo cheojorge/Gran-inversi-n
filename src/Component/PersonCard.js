7135bc50cad7e3c879433dd91dff1ec391d1f3b9
cheojorge <cheojorge@hotmail.com>
2022-01-14 22:09:51 -0500

    useEfect

--- src/Component/PersonCard.js
+++ src/Component/PersonCard.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const PersonalCards = props => {
+    const [age, setAge] = useState(props.age)
 
+    const plusAge = () =>{
+        setAge(age +1);
+    }
         return(
             <>
                 <h1>{props.lastName}, {props.firstName}</h1>
-                <h3>Age: {props.age}</h3>
+                <h3>Age: {age}</h3>
                 <h3>Hair Color: {props.hairColor}</h3>
+                <button onClick={plusAge}>Birthday Button for {props.firstName} {props.lastName}</button>
+
                 
             </>
         )
